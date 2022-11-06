cd6acb286fe22714f3f81946e2298051b79048dd
Dagna Schmidt <dagdagi889@gmail.com>
2022-11-06 11:51:26 +0100

    fixed positions of btns, scroll up added

--- js/main.js
+++ js/main.js
@@ -39,18 +39,14 @@ if(moreClose) {
     })
 }
 
-
-
+//PORTFOLIO//
 var acc = document.getElementsByClassName("portfolio__title");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("open");
 
-    /* Toggle between hiding and showing the active panel */
     var portfolio__description = this.nextElementSibling;
     if (portfolio__description.style.display === "block") {
         portfolio__description.style.display = "none";
@@ -59,3 +55,11 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 } 
+
+//SCROLL UP//
+function scrollUp(){
+    const scrollUp = document.getElementById('scroll-up');
+    if(this.scrollY >= 200) scrollUp.classList.add('show-scroll'); 
+    else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
